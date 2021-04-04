104f4b01e7fac80ffc850123a2b9a778353ddf7c
John Krueger <johnathankrueger@me.com>
2021-04-03 21:47:24 -0400

    first commit

---
+++ contracts/UpstateCoin.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.5.0;
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * The Sample contract does this and that...
+ */
+contract UpstateCoin is ERC20  {
+    constructor() public {}
+}
+
